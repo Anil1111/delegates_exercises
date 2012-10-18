07f4b707b615f2e3646650ddaf6b7970a2030f45
Stefan Moser <stefanm@iqmetrix.com>
2012-10-18 16:32:50 -0700

    bubble sort implementation for collection

---
+++ FunWithDelegates.Tests/PrettyLameCollectionTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace FunWithDelegates.Tests
+{
+    [TestFixture]
+    public class PrettyLameCollectionTests
+    {
+        [Test]
+        public void sorts_integers()
+        {
+            var myCollection = new PrettyLameCollection<int>();
+            myCollection.Add(5, 1, 4, 2, 8);
+
+            myCollection.Sort();
+
+            CollectionAssert.AreEqual(new[] {1, 2, 4, 5, 8}, myCollection);
+        }
+
+        [Test]
+        public void sorts_string()
+        {
+            var myCollection = new PrettyLameCollection<string>();
+            myCollection.Add("Chris", "Kelly", "Scott", "Ron", "Eric");
+
+            myCollection.Sort();
+
+            CollectionAssert.AreEqual(new[] {"Chris", "Eric", "Kelly", "Ron", "Scott"}, myCollection);
+        }
+    }
+}
---
+++ FunWithDelegates/PrettyLameCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace FunWithDelegates
+{
+    public class PrettyLameCollection<T> : IEnumerable where T : IComparable<T>
+    {
+        private const int DefaultCapacity = 10;
+
+        private readonly T[] _items;
+        private int _currentSize;
+
+        public PrettyLameCollection() : this(DefaultCapacity) {}
+
+        public PrettyLameCollection(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public T this[int i]
+        {
+            get { return _items[i]; }
+        }
+
+        public void Add(T item)
+        {
+            _items[_currentSize++] = item;
+        }
+
+        public void Add(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void Sort()
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (var i = 0; i < _currentSize - 1; i++)
+                {
+                    if (_items[i].CompareTo(_items[i + 1]) > 0)
+                    {
+                        SwapItems(i, i + 1);
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped);
+        }
+
+        private void SwapItems(int index1, int index2)
+        {
+            var tempItem = _items[index1];
+            _items[index1] = _items[index2];
+            _items[index2] = tempItem;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var instanceArray = new T[_currentSize];
+            for (int i = 0; i < _currentSize; i++)
+            {
+                instanceArray[i] = _items[i];
+            }
+
+            return  instanceArray.GetEnumerator();
+        }
+    }
+}
